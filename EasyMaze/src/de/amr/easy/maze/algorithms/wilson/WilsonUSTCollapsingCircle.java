971413ffef3da678a1a10abc43dd124edbb8dbd4
Armin Reichert <armin.reichert@web.de>
2016-09-20 15:58:30 +0200

    Refactoring

--- EasyMaze/src/de/amr/easy/maze/algorithms/wilson/WilsonUSTCollapsingCircle.java
+++ EasyMaze/src/de/amr/easy/maze/algorithms/wilson/WilsonUSTCollapsingCircle.java
@@ -1,5 +1,8 @@
 package de.amr.easy.maze.algorithms.wilson;
 
+import static de.amr.easy.grid.api.GridPosition.CENTER;
+import static java.lang.Math.max;
+
 import de.amr.easy.graph.api.TraversalState;
 import de.amr.easy.graph.impl.DefaultEdge;
 import de.amr.easy.grid.api.ObservableDataGrid2D;
@@ -12,16 +15,17 @@ import de.amr.easy.grid.iterators.shapes.Circle;
  */
 public class WilsonUSTCollapsingCircle<Cell> extends WilsonUST<Cell> {
 
-	public WilsonUSTCollapsingCircle(ObservableDataGrid2D<Cell, DefaultEdge<Cell>, TraversalState> grid) {
+	public WilsonUSTCollapsingCircle(
+			ObservableDataGrid2D<Cell, DefaultEdge<Cell>, TraversalState> grid) {
 		super(grid);
 	}
 
 	@Override
 	public void accept(Cell start) {
-		Cell center = grid.cell(grid.numCols() / 2, grid.numRows() / 2);
 		addCellToTree(start);
-		for (int radius = grid.numCols() - 1; radius >= 0; radius--) {
-			for (Cell walkStart : new Circle<Cell>(grid, center, radius)) {
+		Cell center = grid.cell(CENTER);
+		for (int radius = max(grid.numRows(), grid.numCols()) - 1; radius >= 0; radius--) {
+			for (Cell walkStart : new Circle<>(grid, center, radius)) {
 				if (!isCellInTree(walkStart)) {
 					loopErasedRandomWalk(walkStart);
 				}
