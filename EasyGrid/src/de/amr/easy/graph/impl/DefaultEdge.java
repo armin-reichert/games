9fce0facac98250b0cf786b6cb58fe640e6fdf9b
Armin Reichert <armin.reichert@web.de>
2016-10-01 16:33:53 +0200

    equals + hashCode

--- EasyGrid/src/de/amr/easy/graph/impl/DefaultEdge.java
+++ EasyGrid/src/de/amr/easy/graph/impl/DefaultEdge.java
@@ -50,11 +50,33 @@ public class DefaultEdge<V> implements Edge<V> {
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (o instanceof DefaultEdge<?>) {
-			DefaultEdge<?> edge = (DefaultEdge<?>) o;
-			return edge.u == this.u && edge.v == this.v;
-		}
-		return false;
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((u == null) ? 0 : u.hashCode());
+		result = prime * result + ((v == null) ? 0 : v.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DefaultEdge<?> other = (DefaultEdge<?>) obj;
+		if (u == null) {
+			if (other.u != null)
+				return false;
+		} else if (!u.equals(other.u))
+			return false;
+		if (v == null) {
+			if (other.v != null)
+				return false;
+		} else if (!v.equals(other.v))
+			return false;
+		return true;
 	}
 }
