e59277fc2b02700655b4e8363b496cefb5cf5b01
Armin Reichert <armin.reichert@web.de>
2017-05-16 13:27:28 +0200

    Pinky test app

---
+++ PacMan/src/de/amr/games/pacman/test/PinkyTestApp.java
@@ -0,0 +1,39 @@
+package de.amr.games.pacman.test;
+
+import static de.amr.games.pacman.core.board.Board.NUM_COLS;
+import static de.amr.games.pacman.core.board.Board.NUM_ROWS;
+import static de.amr.games.pacman.theme.PacManTheme.TILE_SIZE;
+
+import de.amr.easy.game.ui.FullScreen;
+import de.amr.games.pacman.core.app.AbstractPacManApp;
+
+/**
+ * The Pinky behavior test app.
+ * 
+ * @author Armin Reichert
+ */
+public class PinkyTestApp extends AbstractPacManApp {
+
+	public static void main(String... args) {
+		PinkyTestApp app = new PinkyTestApp();
+		app.settings.title = "Pinky behaviour test";
+		app.settings.width = NUM_COLS * TILE_SIZE;
+		app.settings.height = NUM_ROWS * TILE_SIZE;
+		app.settings.scale = args.length > 0 ? Float.valueOf(args[0]) / app.settings.height : 1;
+		app.settings.fullScreenOnStart = false;
+		app.settings.fullScreenMode = FullScreen.Mode(800, 600, 16);
+		app.settings.set("drawInternals", true);
+		app.settings.set("drawGrid", true);
+		app.settings.set("drawRoute", true);
+		app.gameLoop.log = false;
+		app.gameLoop.setTargetFrameRate(60);
+		launch(app);
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+		views.add(new PinkyTestScene(this));
+		views.show(PinkyTestScene.class);
+	}
+}
---
+++ PacMan/src/de/amr/games/pacman/test/PinkyTestScene.java
@@ -0,0 +1,93 @@
+package de.amr.games.pacman.test;
+
+import static de.amr.easy.grid.impl.Top4.E;
+import static de.amr.easy.grid.impl.Top4.W;
+import static de.amr.games.pacman.core.board.Board.NUM_COLS;
+import static de.amr.games.pacman.core.board.Board.NUM_ROWS;
+import static de.amr.games.pacman.core.board.TileContent.Energizer;
+import static de.amr.games.pacman.core.board.TileContent.Pellet;
+import static de.amr.games.pacman.core.entities.PacManState.Eating;
+import static de.amr.games.pacman.core.entities.ghost.behaviors.GhostState.Chasing;
+import static de.amr.games.pacman.misc.SceneHelper.drawSprite;
+import static de.amr.games.pacman.play.PlayScene.GHOST_HOUSE_ENTRY;
+import static de.amr.games.pacman.play.PlayScene.PACMAN_HOME;
+import static de.amr.games.pacman.theme.PacManTheme.TILE_SIZE;
+import static java.util.stream.IntStream.range;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.Random;
+
+import de.amr.easy.game.scene.Scene;
+import de.amr.games.pacman.core.board.Board;
+import de.amr.games.pacman.core.entities.PacMan;
+import de.amr.games.pacman.core.entities.ghost.Ghost;
+import de.amr.games.pacman.core.entities.ghost.behaviors.TargetAtTileAheadOfPacMan;
+import de.amr.games.pacman.theme.PacManTheme;
+
+/**
+ * A scene for testing Pinky.
+ * 
+ * @author Armin Reichert
+ */
+public class PinkyTestScene extends Scene<PinkyTestApp> {
+
+	private Board board;
+	private PacMan pacMan;
+	private Ghost pinky;
+	private Random rand = new Random();
+
+	public PinkyTestScene(PinkyTestApp app) {
+		super(app);
+	}
+
+	@Override
+	public void init() {
+		board = new Board(app.assets.text("board.txt").split("\n"));
+
+		pacMan = new PacMan(app, board, PACMAN_HOME);
+		pacMan.setSpeed(8 * TILE_SIZE / app.settings.fps);
+
+		pinky = new Ghost(app, board, GHOST_HOUSE_ENTRY);
+		pinky.control.state(Chasing, new TargetAtTileAheadOfPacMan(pinky, pacMan, 4));
+		pinky.stateAfterFrightened = () -> Chasing;
+		pinky.setColor(Color.PINK);
+		pinky.setName("Pinky");
+		pinky.setAnimated(true);
+		pinky.setSpeed(pacMan.getSpeed() * .9f);
+
+		pacMan.control.changeTo(Eating);
+		pinky.control.changeTo(Chasing);
+	};
+
+	@Override
+	public void update() {
+		pacMan.update();
+		pinky.update();
+		if (pacMan.currentTile().equals(pinky.currentTile())) {
+			pacMan.placeAt(PACMAN_HOME);
+			int dir = rand.nextBoolean() ? E : W;
+			pacMan.setMoveDir(dir);
+			pacMan.setNextMoveDir(dir);
+			pinky.placeAt(GHOST_HOUSE_ENTRY);
+			dir = rand.nextBoolean() ? W : E;
+			pinky.setMoveDir(dir); // TODO without this, ghost might get stuck
+			pinky.setNextMoveDir(dir);
+		}
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		PacManTheme theme = app.selectedTheme();
+		drawSprite(g, 3, 0, theme.getBoardSprite());
+		range(4, NUM_ROWS - 3).forEach(row -> range(0, NUM_COLS).forEach(col -> {
+			if (board.contains(row, col, Pellet)) {
+				drawSprite(g, row, col, theme.getPelletSprite());
+			} else if (board.contains(row, col, Energizer)) {
+				drawSprite(g, row, col, theme.getEnergizerSprite());
+			}
+		}));
+		pacMan.draw(g);
+		pinky.draw(g);
+	}
+}
