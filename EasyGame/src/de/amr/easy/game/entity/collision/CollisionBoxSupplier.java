2bca3f781f378c63e4457179f6e30dc3cb41bbbd
Armin Reichert <armin.reichert@web.de>
2017-08-21 16:34:03 +0200

    moved

--- EasyGame/src/de/amr/easy/game/entity/collision/CollisionBoxSupplier.java
+++
@@ -1,8 +0,0 @@
-package de.amr.easy.game.entity.collision;
-
-import java.awt.geom.Rectangle2D;
-
-public interface CollisionBoxSupplier {
-
-	public Rectangle2D getCollisionBox();
-}
--- EasyGame/src/de/amr/easy/game/sprite/AnimationMode.java
+++
@@ -1,5 +0,0 @@
-package de.amr.easy.game.sprite;
-
-public enum AnimationMode {
-	LEFT_TO_RIGHT, CYCLIC, BACK_AND_FORTH;
-}
