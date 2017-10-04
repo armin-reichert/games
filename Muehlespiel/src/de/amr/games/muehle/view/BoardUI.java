46f8ee91553736168def461320f6aa85ba221c5e
Armin Reichert <armin.reichert@web.de>
2017-10-04 12:22:54 +0200

    improved

--- Muehlespiel/src/de/amr/games/muehle/view/BoardUI.java
+++ Muehlespiel/src/de/amr/games/muehle/view/BoardUI.java
@@ -3,7 +3,6 @@ package de.amr.games.muehle.view;
 import static de.amr.easy.game.math.Vector2f.dist;
 import static de.amr.games.muehle.model.board.Board.NUM_POS;
 import static de.amr.games.muehle.model.board.Board.positions;
-import static java.lang.Math.abs;
 import static java.lang.Math.round;
 import static java.util.stream.Collectors.minBy;
 
@@ -31,8 +30,10 @@ import de.amr.games.muehle.model.board.StoneColor;
  */
 public class BoardUI extends GameEntity {
 
-	private static final int[] GRID_X = { 0, 3, 6, 1, 3, 5, 2, 3, 4, 0, 1, 2, 4, 5, 6, 2, 3, 4, 1, 3, 5, 0, 3, 6 };
-	private static final int[] GRID_Y = { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6 };
+	private static final int[] GRID_X = { 0, 3, 6, 1, 3, 5, 2, 3, 4, 0, 1, 2, 4, 5, 6, 2, 3, 4, 1, 3,
+			5, 0, 3, 6 };
+	private static final int[] GRID_Y = { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5,
+			5, 6, 6, 6 };
 
 	private final Board board;
 	private final Stone[] stones;
@@ -56,7 +57,8 @@ public class BoardUI extends GameEntity {
 		this.size = size;
 		this.gridSize = size / 6;
 		this.posRadius = size / 60;
-		positions().forEach(p -> center[p] = Vector2f.smul(gridSize, Vector2f.of(GRID_X[p], GRID_Y[p])));
+		positions()
+				.forEach(p -> center[p] = Vector2f.smul(gridSize, Vector2f.of(GRID_X[p], GRID_Y[p])));
 		this.numbersFont = new Font("Arial", Font.PLAIN, size / 30);
 	}
 
@@ -118,7 +120,7 @@ public class BoardUI extends GameEntity {
 		return center[p];
 	}
 
-	public OptionalInt findNearestBoardPosition(int x, int y, int radius) {
+	private OptionalInt findBoardPosition(float x, float y, int radius) {
 		Vector2f point = Vector2f.of(x, y);
 		/*@formatter:off*/
 		Optional<Integer> opt = positions().boxed()
@@ -128,10 +130,20 @@ public class BoardUI extends GameEntity {
 		return opt.isPresent() ? OptionalInt.of(opt.get()) : OptionalInt.empty();
 	}
 
-	public OptionalInt findBoardPosition(int x, int y) {
-		int boardX = abs(round(x - tf.getX()));
-		int boardY = abs(round(y - tf.getY()));
-		return findNearestBoardPosition(boardX, boardY, gridSize / 2);
+	public OptionalInt findBoardPosition(float x, float y) {
+		x -= tf.getX();
+		y -= tf.getY();
+		if (x < 0) {
+			x = 0;
+		} else if (x > size) {
+			x = size;
+		}
+		if (y < 0) {
+			y = 0;
+		} else if (y > size) {
+			y = size;
+		}
+		return findBoardPosition(x, y, gridSize / 2);
 	}
 
 	public void showPositionNumbers() {
@@ -160,19 +172,23 @@ public class BoardUI extends GameEntity {
 		g.setColor(lineColor);
 		g.setStroke(new BasicStroke(posRadius / 2));
 		positions().forEach(from -> Board.neighbors(from).filter(to -> to > from).forEach(to -> {
-			g.drawLine(center[from].roundedX(), center[from].roundedY(), center[to].roundedX(), center[to].roundedY());
+			g.drawLine(center[from].roundedX(), center[from].roundedY(), center[to].roundedX(),
+					center[to].roundedY());
 		}));
 		// Positions
 		positions().forEach(p -> {
 			g.setColor(lineColor);
 			aa_on(g);
-			g.fillOval(center[p].roundedX() - posRadius, center[p].roundedY() - posRadius, 2 * posRadius, 2 * posRadius);
+			g.fillOval(center[p].roundedX() - posRadius, center[p].roundedY() - posRadius, 2 * posRadius,
+					2 * posRadius);
 			aa_off(g);
 			if (numbersOn) {
 				g.setFont(numbersFont);
-				g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+				g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+						RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 				g.drawString(String.valueOf(p), center[p].x + 2 * posRadius, center[p].y + 4 * posRadius);
-				g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
+				g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+						RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
 			}
 		});
 		// Stones
@@ -184,26 +200,28 @@ public class BoardUI extends GameEntity {
 		g.translate(tf.getX(), tf.getY());
 		g.setColor(color);
 		aa_on(g);
-		g.fillOval(round(center[p].x) - posRadius / 2, round(center[p].y) - posRadius / 2, posRadius, posRadius);
+		g.fillOval(round(center[p].x) - posRadius / 2, round(center[p].y) - posRadius / 2, posRadius,
+				posRadius);
 		aa_off(g);
 		g.translate(-tf.getX(), -tf.getY());
 	}
 
 	public void markRemovableStones(Graphics2D g, StoneColor stoneColor) {
 		boolean allStonesInMills = board.allStonesInMills(stoneColor);
-		board.positions(stoneColor).filter(p -> allStonesInMills || !board.inMill(p, stoneColor)).forEach(p -> {
-			stoneAt(p).ifPresent(stone -> {
-				float offsetX = tf.getX() + stone.tf.getX() - stone.getWidth() / 2;
-				float offsetY = tf.getY() + stone.tf.getY() - stone.getHeight() / 2;
-				// draw red cross
-				g.translate(offsetX, offsetY);
-				g.setColor(Color.RED);
-				aa_on(g);
-				g.drawLine(0, 0, stone.getWidth(), stone.getHeight());
-				g.drawLine(0, stone.getHeight(), stone.getWidth(), 0);
-				aa_off(g);
-				g.translate(-offsetX, -offsetY);
-			});
-		});
+		board.positions(stoneColor).filter(p -> allStonesInMills || !board.inMill(p, stoneColor))
+				.forEach(p -> {
+					stoneAt(p).ifPresent(stone -> {
+						float offsetX = tf.getX() + stone.tf.getX() - stone.getWidth() / 2;
+						float offsetY = tf.getY() + stone.tf.getY() - stone.getHeight() / 2;
+						// draw red cross
+						g.translate(offsetX, offsetY);
+						g.setColor(Color.RED);
+						aa_on(g);
+						g.drawLine(0, 0, stone.getWidth(), stone.getHeight());
+						g.drawLine(0, stone.getHeight(), stone.getWidth(), 0);
+						aa_off(g);
+						g.translate(-offsetX, -offsetY);
+					});
+				});
 	}
 }
