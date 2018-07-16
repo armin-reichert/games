c02847d86ce7a5d7f51cd5a5fdfa028e686f20ed
Armin Reichert <armin.reichert@web.de>
2018-07-16 15:08:03 +0200

    improved

--- PacMan2.0/src/de/amr/games/pacman/PacManApp.java
+++ PacMan2.0/src/de/amr/games/pacman/PacManApp.java
@@ -7,18 +7,21 @@ import de.amr.games.pacman.model.Maze;
 
 public class PacManApp extends Application {
 
+	/** Tile size of the board. */
+	public static int TS = 16;
+
 	public static void main(String[] args) {
+		if (args.length > 0) {
+			TS = Integer.parseInt(args[0]);
+		}
 		launch(new PacManApp());
 	}
-
-	/** Tile size of the board. */
-	public static final int TS = 16;
 
 	public PacManApp() {
 		Maze maze = Maze.of(Assets.text("maze.txt"));
 		settings.width = maze.numCols() * TS;
 		settings.height = (maze.numRows() + 5) * TS;
-		settings.scale = 1.25f;
+		settings.scale = 1f;
 		settings.title = String.format("PacMan 2.0 (%d x %d * %.2f)", settings.width, settings.height, settings.scale);
 	}
 
