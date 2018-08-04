12d254e426fdf9c4573e0b03b92bfaef4cf7c04c
armin <armin@X230.fritz.box>
2018-08-04 10:11:46 +0200

    doc

--- PacMan2.0/src/de/amr/statemachine/StateObject.java
+++ PacMan2.0/src/de/amr/statemachine/StateObject.java
@@ -4,29 +4,31 @@ import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 
 /**
- * A state of a finite state machine.
+ * Implementation of a state in a finite state machine.
  * 
  * @author Armin Reichert
  */
 public class StateObject<S, E> {
 
 	/** Constant for defining an unlimited duration. */
-	public static final int FOREVER = Integer.MAX_VALUE;
+	public static final int UNLIMITED = Integer.MAX_VALUE;
 
+	/** The state machine this state belongs to. */
 	public final StateMachine<S, E> sm;
 
+	/** The label used to identify this state. */
 	public final S label;
 
-	/** The action performed when entering this state. */
+	/** The client code executed when entering this state. */
 	public Consumer<StateObject<S, E>> entry;
 
-	/** The action performed when an update occurs during this state. */
+	/** The client code executed when an update occurs for this state. */
 	public Consumer<StateObject<S, E>> update;
 
-	/** The action performed when leaving this state. */
+	/** The client code executed when leaving this state. */
 	public Consumer<StateObject<S, E>> exit;
 
-	/** The duration until this state terminates. */
+	/** The duration until this state times out. */
 	private int duration;
 
 	/** Ticks remaining until time-out */
@@ -38,8 +40,7 @@ public class StateObject<S, E> {
 	public StateObject(StateMachine<S, E> sm, S label) {
 		this.sm = sm;
 		this.label = label;
-		duration = FOREVER;
-		resetTimer();
+		remaining = duration = UNLIMITED;
 	}
 
 	void doEntry() {
@@ -104,20 +105,16 @@ public class StateObject<S, E> {
 
 	private final BooleanSupplier ALWAYS_TRUE = () -> true;
 	private final Consumer<StateTransition<S, E>> NO_ACTION = t -> {
-
 	};
 
 	/**
-	 * Defines a transition between the given states which can be fired only if the given condition
-	 * holds. If the transition is executed the given action is also executed. This happens before the
-	 * new state is entered.
+	 * Defines a transition to the given state guarded by the given condition. If
+	 * the transition is executed the given action is executed just before the new
+	 * state is entered.
 	 * 
-	 * @param to
-	 *                    the target state
-	 * @param condition
-	 *                    the condition which must hold
-	 * @param action
-	 *                    code which will be executed when this transition occurs
+	 * @param to        the target state
+	 * @param condition a condition (guard)
+	 * @param action    code which will be executed when this transition occurs
 	 */
 	public StateObject<S, E> change(S to, BooleanSupplier condition, Consumer<StateTransition<S, E>> action) {
 		sm.addTransition(label, to, condition, action);
@@ -125,13 +122,10 @@ public class StateObject<S, E> {
 	}
 
 	/**
-	 * Defines a transition between the given states which can be fired only if the given condition
-	 * holds.
+	 * Defines a transition to the given state guarded by the given condition.
 	 * 
-	 * @param to
-	 *                    the target state
-	 * @param condition
-	 *                    the condition which must hold
+	 * @param to        the target state
+	 * @param condition a condition (guard)
 	 */
 	public StateObject<S, E> change(S to, BooleanSupplier condition) {
 		sm.addTransition(label, to, condition, NO_ACTION);
@@ -139,10 +133,9 @@ public class StateObject<S, E> {
 	};
 
 	/**
-	 * Defines a transition between the given states which can always be fired.
+	 * Defines a transition to the given state which can always be fired.
 	 * 
-	 * @param to
-	 *             the target state
+	 * @param to the target state
 	 */
 	public StateObject<S, E> change(S to) {
 		sm.addTransition(label, to, ALWAYS_TRUE, NO_ACTION);
@@ -150,25 +143,22 @@ public class StateObject<S, E> {
 	};
 
 	/**
-	 * Defines a transition between the given states which can be fired if the source state got a
-	 * timeout.
+	 * Defines a transition to the given state which can be fired if this state is
+	 * timed out.
 	 * 
-	 * @param to
-	 *             the target state
+	 * @param to the target state
 	 */
 	public StateObject<S, E> changeOnTimeout(S to) {
-		sm.addTransition(label, to, () -> isTerminated(), NO_ACTION);
+		sm.addTransition(label, to, this::isTerminated, NO_ACTION);
 		return this;
 	}
 
 	/**
-	 * Defines a transition between the given states which can be fired if the source state got a
-	 * timeout and the given condition holds.
+	 * Defines a transition to the given state which can be fired if this state is
+	 * timed out and the given condition holds.
 	 * 
-	 * @param to
-	 *                    the target state
-	 * @param condition
-	 *                    some condition
+	 * @param to        the target state
+	 * @param condition some condition
 	 */
 	public StateObject<S, E> changeOnTimeout(S to, BooleanSupplier condition) {
 		sm.addTransition(label, to, () -> isTerminated() && condition.getAsBoolean(), NO_ACTION);
@@ -176,13 +166,11 @@ public class StateObject<S, E> {
 	}
 
 	/**
-	 * Defines a transition between the given states which can be fired if the source state got a
-	 * timeout.
+	 * Defines a transition to the given state which can be fired if this state is
+	 * timed out.
 	 * 
-	 * @param to
-	 *                 the target state
-	 * @param action
-	 *                 code which will be executed when this transition occurs
+	 * @param to     the target state
+	 * @param action code which will be executed when this transition occurs
 	 */
 	public StateObject<S, E> changeOnTimeout(S to, Consumer<StateTransition<S, E>> action) {
 		sm.addTransition(label, to, this::isTerminated, action);
@@ -190,10 +178,10 @@ public class StateObject<S, E> {
 	}
 
 	/**
-	 * Defines a transition between the given states which can be fired if this state got a timeout.
+	 * Defines a transition without state change which can be fired if this state is
+	 * timed out.
 	 * 
-	 * @param action
-	 *                 code which will be executed when this transition occurs
+	 * @param action code which will be executed when this transition occurs
 	 */
 	public StateObject<S, E> onTimeout(Consumer<StateTransition<S, E>> action) {
 		sm.addTransition(label, label, this::isTerminated, action);
@@ -201,15 +189,12 @@ public class StateObject<S, E> {
 	}
 
 	/**
-	 * Defines a transition between the given states which can be fired if the source state got a
-	 * timeout and the given condition holds.
+	 * Defines a transition to the given state which can be fired if this state is
+	 * timed out and the given condition holds.
 	 * 
-	 * @param to
-	 *                    the target state
-	 * @param condition
-	 *                    some condition
-	 * @param action
-	 *                    code which will be executed when this transition occurs
+	 * @param to        the target state
+	 * @param condition some condition
+	 * @param action    code which will be executed when this transition occurs
 	 */
 	public StateObject<S, E> changeOnTimeout(S to, BooleanSupplier condition, Consumer<StateTransition<S, E>> action) {
 		sm.addTransition(label, to, () -> isTerminated() && condition.getAsBoolean(), action);
@@ -217,13 +202,11 @@ public class StateObject<S, E> {
 	}
 
 	/**
-	 * Defines a transition in this state which can be fired if this state got a timeout and the given
-	 * condition holds.
+	 * Defines a transition without state change which can be fired if this state is
+	 * timed out and the given condition holds.
 	 * 
-	 * @param condition
-	 *                    some condition
-	 * @param action
-	 *                    code which will be executed when this transition occurs
+	 * @param condition some condition
+	 * @param action    code which will be executed when this transition occurs
 	 */
 	public StateObject<S, E> onTimeout(BooleanSupplier condition, Consumer<StateTransition<S, E>> action) {
 		sm.addTransition(label, label, () -> isTerminated() && condition.getAsBoolean(), action);
@@ -231,13 +214,11 @@ public class StateObject<S, E> {
 	}
 
 	/**
-	 * Defines a transition between the given states which can be fired only if the next event matches
-	 * the transition.
+	 * Defines a transition to the given state which can be fired if the current
+	 * event matches the given type.
 	 * 
-	 * @param eventType
-	 *                    type of events matching the transition
-	 * @param to
-	 *                    the target state
+	 * @param eventType type of events matching the transition
+	 * @param to        the target state
 	 */
 	public StateObject<S, E> changeOnInput(Class<? extends E> eventType, S to) {
 		sm.addTransition(label, to, () -> sm.isEventMatching(eventType), NO_ACTION);
@@ -245,13 +226,11 @@ public class StateObject<S, E> {
 	}
 
 	/**
-	 * Defines a transition in this state which can be fired only if the next event matches the
-	 * transition.
+	 * Defines a transition without state change which can be fired if the current
+	 * event matches the given type.
 	 * 
-	 * @param eventType
-	 *                    type of events matching the transition
-	 * @param to
-	 *                    the target state
+	 * @param eventType type of events matching the transition
+	 * @param to        the target state
 	 */
 	public StateObject<S, E> onInput(Class<? extends E> eventType) {
 		sm.addTransition(label, label, () -> sm.isEventMatching(eventType), NO_ACTION);
@@ -259,59 +238,65 @@ public class StateObject<S, E> {
 	}
 
 	/**
-	 * Defines a transition between the given states which can be fired only if the next event matches
-	 * the transition and the given condition holds.
+	 * Defines a transition to the given state which can be fired if the current
+	 * event matches the given type and the given condition holds.
 	 * 
-	 * @param eventType
-	 *                    type of events matching the transition
-	 * @param to
-	 *                    the target state
-	 * @param condition
-	 *                    some condition
+	 * @param eventType type of events matching the transition
+	 * @param to        the target state
+	 * @param condition some condition
 	 */
 	public StateObject<S, E> changeOnInput(Class<? extends E> eventType, S to, BooleanSupplier condition) {
 		sm.addTransition(label, to, () -> sm.isEventMatching(eventType) && condition.getAsBoolean(), NO_ACTION);
 		return this;
 	}
 
+	/**
+	 * Defines a transition without state change which can be fired if the current
+	 * event matches the given type and the given condition holds.
+	 * 
+	 * @param eventType type of events matching the transition
+	 * @param to        the target state
+	 * @param condition some condition
+	 */
 	public StateObject<S, E> onInput(Class<? extends E> eventType, BooleanSupplier condition) {
 		sm.addTransition(label, label, () -> sm.isEventMatching(eventType) && condition.getAsBoolean(), NO_ACTION);
 		return this;
 	}
 
 	/**
-	 * Defines a transition between the given states which can be fired only if the next event matches
-	 * the transition.
+	 * Defines a transition to the given state which can be fired if the current
+	 * event matches the given type.
 	 * 
-	 * @param eventType
-	 *                    type of events matching the transition
-	 * @param to
-	 *                    the target state
-	 * @param action
-	 *                    performed action
+	 * @param eventType type of events matching the transition
+	 * @param to        the target state
+	 * @param action    performed action
 	 */
 	public StateObject<S, E> changeOnInput(Class<? extends E> eventType, S to, Consumer<StateTransition<S, E>> action) {
 		sm.addTransition(label, to, () -> sm.isEventMatching(eventType), action);
 		return this;
 	}
 
+	/**
+	 * Defines a transition without state change which can be fired if the current
+	 * event matches the given type.
+	 * 
+	 * @param eventType type of events matching the transition
+	 * @param to        the target state
+	 * @param action    performed action
+	 */
 	public StateObject<S, E> onInput(Class<? extends E> eventType, Consumer<StateTransition<S, E>> action) {
 		sm.addTransition(label, label, () -> sm.isEventMatching(eventType), action);
 		return this;
 	}
 
 	/**
-	 * Defines a transition between the given states which can be fired only if the next event matches
-	 * the transition.
+	 * Defines a transition to the given state which can be fired if the current
+	 * event matches the given type and the given condition holds.
 	 * 
-	 * @param eventType
-	 *                    type of events matching the transition
-	 * @param to
-	 *                    the target state
-	 * @param condition
-	 *                    some condition
-	 * @param action
-	 *                    performed action
+	 * @param eventType type of events matching the transition
+	 * @param to        the target state
+	 * @param condition some condition
+	 * @param action    performed action
 	 */
 	public StateObject<S, E> changeOnInput(Class<? extends E> eventType, S to, BooleanSupplier condition,
 			Consumer<StateTransition<S, E>> action) {
@@ -319,6 +304,15 @@ public class StateObject<S, E> {
 		return this;
 	}
 
+	/**
+	 * Defines a transition without state change which can be fired if the current
+	 * event matches the given type and the given condition holds.
+	 * 
+	 * @param eventType type of events matching the transition
+	 * @param to        the target state
+	 * @param condition some condition
+	 * @param action    performed action
+	 */
 	public StateObject<S, E> onInput(Class<? extends E> eventType, BooleanSupplier condition,
 			Consumer<StateTransition<S, E>> action) {
 		sm.addTransition(label, label, () -> sm.isEventMatching(eventType) && condition.getAsBoolean(), action);
