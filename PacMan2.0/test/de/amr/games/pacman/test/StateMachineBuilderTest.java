3c88824993f9c171c6aaa969b660ae72fe5a22e7
Armin Reichert <armin.reichert@web.de>
2018-08-06 16:00:52 +0200

    test

--- PacMan2.0/test/de/amr/games/pacman/test/StateMachineBuilderTest.java
+++ PacMan2.0/test/de/amr/games/pacman/test/StateMachineBuilderTest.java
@@ -1,29 +1,47 @@
 package de.amr.games.pacman.test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import de.amr.statemachine.StateMachine;
+import de.amr.statemachine.StateObject;
 
 public class StateMachineBuilderTest {
+	
+	interface Event {
+	}
+	
+	class EventX implements Event {
+		
+	}
+
+	class StateB extends StateObject<String, Event> {
+	}
 
 	@Test
 	public void test() {
 		/*@formatter:off*/
-		StateMachine<String, Integer> sm = StateMachine.builder(String.class, Integer.class)
+		StateMachine<String, Event> sm = StateMachine.builder(String.class, Event.class)
 			.description("SampleFSM")
 			.initialState("A")
 			.states()
 				.state("A")
-				.state("B").impl(null)
+				.state("B").impl(StateB::new)
 				.state("C")
 			.transitions()
-				.change("A", "B").when(() -> 10 > 9).act(t -> {
+				.change("A", "B").on(EventX.class).act(t -> {
 					System.out.println("Action");
 				}).build()
 				.keep("B").when(() -> 10 > 9).build()
 			.buildStateMachine();
 		/*@formatter:on*/
+
+		sm.init();
+		assertTrue(sm.currentState().equals("A"));
 		
-		//TODO add assertions
+		sm.enqueue(new EventX());
+		sm.update();
+		assertTrue(sm.currentState().equals("B"));
 	}
 }
