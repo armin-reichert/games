fa200a6ba7bb120eb67d17d7ee25f687a91e3c6f
Armin Reichert <armin.reichert@web.de>
2019-02-17 12:28:11 +0100

    improved

--- 15Puzzle/src/main/java/de/amr/games/puzzle15/solver/SolverIDDFS.java
+++ 15Puzzle/src/main/java/de/amr/games/puzzle15/solver/SolverIDDFS.java
@@ -10,13 +10,19 @@ public class SolverIDDFS implements Solver {
 	static final int MAX_DEPTH = 50;
 
 	private SolverDepthLimitedDFS dls;
+	private int maxFrontierSize;
+	private long totalRuntime;
 
 	@Override
 	public Optional<List<Node>> solve(Puzzle15 puzzle) throws SolverGivingUpException {
+		maxFrontierSize = 0;
+		totalRuntime = 0;
 		for (int depth = 0; depth <= MAX_DEPTH; ++depth) {
 			dls = new SolverDepthLimitedDFS(depth);
-			System.out.println("Running DLS of depth " + depth);
+			System.out.println("Running DLS including depth " + depth);
 			Optional<List<Node>> solution = dls.solve(puzzle);
+			maxFrontierSize = Math.max(maxFrontierSize, dls.getMaxFrontierSize());
+			totalRuntime += dls.getRunningTime();
 			if (solution.isPresent()) {
 				return solution;
 			}
@@ -26,11 +32,11 @@ public class SolverIDDFS implements Solver {
 
 	@Override
 	public int getMaxFrontierSize() {
-		return dls != null ? dls.getMaxFrontierSize() : 0;
+		return maxFrontierSize;
 	}
 
 	@Override
 	public long getRunningTime() {
-		return dls != null ? dls.getRunningTime() : 0;
+		return totalRuntime;
 	}
 }
